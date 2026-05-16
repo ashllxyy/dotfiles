5882e39bd718f9e6048bd2d763df2912ec0f3c90
Arunesh Unnithan <aruneshunnithan@gmail.com>
2026-05-16 20:51:23 +0530

    theme update

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,11 +3,6 @@ return {
 	dependencies = { "rafamadriz/friendly-snippets" },
 	version = "1.*",
 	opts = {
-		-- All presets have the following mappings:
-		-- C-space: Open menu or open docs if already open
-		-- C-n/C-p or Up/Down: Select next/previous item
-		-- C-e: Hide menu
-		-- C-k: Toggle signature help (if signature.enabled = true)
 		keymap = { preset = "default" },
 		appearance = {
 			nerd_font_variant = "mono",
