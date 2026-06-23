b85be3ca702d419ae906d33270ec375044a8ada3
Arunesh Unnithan <aruneshunnithan@gmail.com>
2026-06-23 16:55:35 +0530

    fix: nvim - telescope build for linux

--- nvim/lua/plugins/telescope/telescope.lua
+++ nvim/lua/plugins/telescope/telescope.lua
@@ -1,36 +1,37 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			{
-				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "mingw32-make",
-			},
-		},
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        -- build = "mingw32-make", -- this is for windows
+        build = "make", -- for linux
+      },
+    },
 
-		config = function()
-			local telescope = require("telescope")
+    config = function()
+      local telescope = require("telescope")
 
-			telescope.setup()
+      telescope.setup()
 
-			telescope.load_extension("fzf")
+      telescope.load_extension("fzf")
 
-			vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
-			vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+      vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+      vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
+    end,
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+      require("telescope").load_extension("ui-select")
+    end,
+  },
 }
