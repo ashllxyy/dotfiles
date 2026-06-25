6d0e475f9d72ac5bf14df70af00db817195e81a2
Arunesh Unnithan <aruneshunnithan@gmail.com>
2026-06-25 15:06:41 +0530

    fix: waybar - minor fixes

--- waybar/scripts/media_player.sh
+++ waybar/scripts/media_player.sh
@@ -3,9 +3,9 @@
 "$HOME/.local/bin/zscroll" \
   --delay 0.3 \
   --length 60 \
-  --match-command "playerctl status --player=%any 2>/dev/null || echo ERROR_STATUS" \
+  --match-command "playerctl status --player=%any 2>/dev/null" \
   --match-text "Playing" "--before-text '▶ '" \
   --match-text "Paused" "--before-text '⏸ ' --scroll 0" \
   --update-check true \
-  "playerctl metadata --player=%any --format '{{ artist }} - {{ title }}' 2>/dev/null || echo ERROR_META" &
+  "playerctl metadata --player=%any --format '{{ artist }} - {{ title }}' 2>/dev/null" &
 wait
