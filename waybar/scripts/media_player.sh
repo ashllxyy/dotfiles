d96a4588ba92b785f4f62bd461f592b48f08b995
Arunesh Unnithan <aruneshunnithan@gmail.com>
2026-06-25 12:14:06 +0530

    fix: waybar - zscroll not detecting on boot

--- waybar/scripts/media_player.sh
+++ waybar/scripts/media_player.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-zscroll \
-  --before-text "♪ " \
+"$HOME/.local/bin/zscroll" \
   --delay 0.3 \
-  --length 40 \
-  --match-command "playerctl status --player=%any 2>/dev/null" \
+  --length 60 \
+  --match-command "playerctl status --player=%any 2>/dev/null || echo ERROR_STATUS" \
   --match-text "Playing" "--before-text '▶ '" \
   --match-text "Paused" "--before-text '⏸ ' --scroll 0" \
   --update-check true \
-  "playerctl metadata --player=%any --format '{{ artist }} - {{ title }}' 2>/dev/null" &
+  "playerctl metadata --player=%any --format '{{ artist }} - {{ title }}' 2>/dev/null || echo ERROR_META" &
 wait
